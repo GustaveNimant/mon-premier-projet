77a90be4fa2fb5280f686a6b5f2c8ba79ee0005b
Emile Achadde <achadde@iabloko.fecamp>
2019-06-07 17:11:07 +0200

    navigation introduite. scss pas correcte

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { AppareilService } from './services/appareil.service';
+
 
 @Component({
     selector: 'app-root',
@@ -7,43 +7,7 @@ import { AppareilService } from './services/appareil.service';
     styleUrls: ['./app.component.scss']
 })
 
-export class AppComponent implements OnInit {
+export class AppComponent {
 
-    isAuth = false;
-
-    lastUpdate = new Promise((resolve, reject) => {
-	const date = new Date();
-
-	setTimeout(
-	    () => {
-		resolve(date);
-	    }, 2000
-	);
-    });
-
-    appareils : any[];
-
-    constructor(private appareilService : AppareilService) {
-	setTimeout(
-	    () => {
-		this.isAuth = true;
-	    }, 4000
-	);
-    }
-
-    ngOnInit () { /* après constructor. avant les autre méthodes */
-	this.appareils = this.appareilService.appareils;
-    }
-    
-    onAllumer() {
-	this.appareilService.switchOnAll();
-    }
-    
-    onEteindre() {
-	if(confirm('Etes-vous sûr de vouloir éteindre tous vos appareils ?')) {
-	    this.appareilService.switchOffAll();
-	} else {
-	    return null;
-	} 
-    }
+    constructor (){}
 }

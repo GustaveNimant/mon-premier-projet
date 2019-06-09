910038dd62c40e18d260d00451e53083a12482d8
Emile Achadde <achadde@iabloko.fecamp>
2019-06-09 12:38:42 +0200

    Observez les données avec RxJS video 4:50 observer la connexion infinie

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/interval';
 
 @Component({
     selector: 'app-root',
@@ -6,7 +8,25 @@ import { Component } from '@angular/core';
     styleUrls: ['./app.component.scss']
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+    secondes: number;
 
     constructor (){}
+
+    ngOnInit() {
+	const counter = Observable.interval(1000);
+	counter.subscribe(
+	    (value) => {
+		this.secondes = value;
+	    },
+	    (error) => {
+		console.log('Uh-oh, an error occurred! : ' + error);
+	    },
+	    () => {
+		console.log('Observable complete!');
+	    }
+	);
+    }
+    
 }
